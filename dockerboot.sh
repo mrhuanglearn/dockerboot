0ce00da79020a264fb7f8a2e353c3903143931c2
mrhuanglearn <huang854000316@gmail.com>
2021-07-02 11:51:36 +0800

    Update dockerboot.sh

--- dockerboot.sh
+++ dockerboot.sh
@@ -3,6 +3,7 @@
 for i in `sudo docker service ls |grep mobile-platform |awk '{print $2}' | sort -r`;
 do
 output=`sudo docker service scale $i=0&&sudo docker service scale $i=1`;
+echo $output;
 done
 echo "===========脚本执行完毕=================";
 exit 1;
