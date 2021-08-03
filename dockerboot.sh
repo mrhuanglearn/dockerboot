b9d74789756c9cdb3eaa4932b38bdc4a2e049794
mrhuanglearn <huang854000316@gmail.com>
2021-08-03 18:16:14 +0800

    Update dockerboot.sh

--- dockerboot.sh
+++ dockerboot.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+FILE_DIR='/platform/platform-multi/apps/'
+MOBILE_CONTROLLER=`ls $FILE_DIR | grep 'controller-1.1.0'`
+MOBILE_SERVICE=`ls $FILE_DIR | grep 'service-1.1.0'`
+
+if [ -f $FILE_DIR$MOBILE_CONTROLLER ]; then
+sudo mv $FILE_DIR$MOBILE_CONTROLLER 'ieep2-mobile-platform-controller.jar'
+fi
+
+
+if [ -f $FILE_DIR$MOBILE_SERVICE ]; then
+sudo mv $FILE_DIR$MOBILE_SERVICE 'ieep2-mobile-platform-service.jar'
+
 for i in `sudo docker service ls |grep mobile-platform |awk '{print $2}' | sort -r`;
 do
 output=`sudo docker service scale $i=0&&sudo docker service scale $i=1`;
